887e359697a6e961939e856b7f3a462f82b53e89
mushtaqsayyed <43338423+mushtaqsayyed@users.noreply.github.com>
2018-09-27 19:14:31 +0530

    rest status api

---
+++ restAPistatus.js
@@ -0,0 +1,23 @@
+var https = require('https');
+var user= 'mushtaqsayyed1';
+var pass= 'NC6Df9KsTAxJAmJArf7C';
+
+var options = {
+host: 'api.browserstack.com',
+port: 443,
+path: '/automate/plan.json',
+// authentication headers
+headers: {
+'Authorization': 'Basic ' + new Buffer(user + ':' + pass).toString('base64')
+}
+};
+https.get(options, function(res){
+var body="";
+res.on('data',function(chunk){
+body+= chunk;
+})
+res.on('end',function(){
+var result = JSON.parse(body);
+console.log(result);
+})
+}).end();
